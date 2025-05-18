568ad05ca2ad97d1f832296b58de60629195cdfc
Marcos Garcia <marcosxtk130@hotmail.com>
2025-05-18 10:45:25 -0300

    atualizacao nivel aventureiro

--- CartasSuperTrunfo1.c
+++ CartasSuperTrunfo1.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+void imprimeDados(int numeroDaCarta, char estadoCarta, char codigoCarta[3], char nomeCarta[50], int populacaoCarta, float areaCarta, float pibCarta, int numeroPontosTuristicosCarta, float densidadePopulacionalCarta, float pibPerCapitaCarta) {
+    //retorna as informações das cartas
+    printf("\nCarta %d\n Estado: %c\n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %.2f km²\n PIB: %.2f bilhoes de reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", numeroDaCarta,estadoCarta, codigoCarta, nomeCarta, populacaoCarta, areaCarta, pibCarta, numeroPontosTuristicosCarta, densidadePopulacionalCarta, pibPerCapitaCarta);   
+    return;
+}
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+void coletaDados() {
+
     char estadoCartaUm, estadoCartaDois, codigoCartaUm[3], codigoCartaDois[3], nomeCartaUm[50], nomeCartaDois[50];
     int populacaoCartaUm, populacaoCartaDois, numeroPontosTuristicosCartaUm, numeroPontosTuristicosCartaDois;
-    float areaCartaUm, areaCartaDois, pibCartaUm, pibCartaDois;
+    float areaCartaUm, areaCartaDois, pibCartaUm, pibCartaDois, densidadePopulacionalCartaUm, densidadePopulacionalCartaDois, pibPerCapitaCartaUm, pibPerCapitaCartaDois;
 
 
     //Coleta as informações da carta 1
@@ -32,7 +23,7 @@ int main() {
     scanf("%s",nomeCartaUm);
     printf("Informe a Populacao: ");
     scanf("%d",&populacaoCartaUm);
-    printf("Informe o Area: ");
+    printf("Informe a Area: ");
     scanf("%f",&areaCartaUm);
     printf("Informe o PIB: ");
     scanf("%f",&pibCartaUm);
@@ -40,7 +31,7 @@ int main() {
     scanf("%d",&numeroPontosTuristicosCartaUm);
 
     //Coleta as informações da carta 2
-    printf("Agora insira as informacoes da Carta 2:");
+    printf("\nAgora insira as informacoes da Carta 2:");
     printf("\nInforme o Estado: ");
     scanf(" %c",&estadoCartaDois);
     printf("Informe o Codigo: ");
@@ -49,15 +40,25 @@ int main() {
     scanf("%s",nomeCartaDois);
     printf("Informe a Populacao: ");
     scanf("%d",&populacaoCartaDois);
-    printf("Informe o Area: ");
+    printf("Informe a Area: ");
     scanf("%f",&areaCartaDois);
     printf("Informe o PIB: ");
     scanf("%f",&pibCartaDois);
     printf("Informe o numero de Pontos Turisticos: ");
     scanf("%d",&numeroPontosTuristicosCartaDois);
 
-    //retorna as informações das cartas
-    printf("Carta 1\n Estado: %c\n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %f km²\n PIB: %f bilhoes de reais\n Numero de Pontos Turisticos: %d\n", estadoCartaUm, codigoCartaUm, nomeCartaUm, populacaoCartaUm, areaCartaUm, pibCartaUm, numeroPontosTuristicosCartaUm);
-    printf("Carta 2\n Estado: %c\n Codigo: %s\n Nome da Cidade: %s\n Populacao: %d\n Area: %f km²\n PIB: %f bilhoes de reais\n Numero de Pontos Turisticos: %d\n", estadoCartaDois, codigoCartaDois, nomeCartaDois, populacaoCartaDois, areaCartaDois, pibCartaDois, numeroPontosTuristicosCartaDois);
-    return 0;
+    //Calcula densidade populacional e pib per Capita
+
+    densidadePopulacionalCartaUm = populacaoCartaUm/ areaCartaUm;
+    densidadePopulacionalCartaDois = populacaoCartaDois / areaCartaDois;
+    pibPerCapitaCartaUm = (pibCartaUm * 1000000000) / populacaoCartaUm;
+    pibPerCapitaCartaDois = (pibCartaDois * 1000000000) / populacaoCartaDois;
+
+    imprimeDados(1, estadoCartaUm, codigoCartaUm, nomeCartaUm, populacaoCartaUm, areaCartaUm, pibCartaUm, numeroPontosTuristicosCartaUm, densidadePopulacionalCartaUm, pibPerCapitaCartaUm);
+    imprimeDados(2, estadoCartaDois, codigoCartaDois, nomeCartaDois, populacaoCartaDois, areaCartaDois, pibCartaDois, numeroPontosTuristicosCartaDois, densidadePopulacionalCartaDois, pibPerCapitaCartaDois);
+}
+
+int main() {
+    coletaDados();
+return 0;
 }
